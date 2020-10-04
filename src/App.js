2f033a260691edf7de54ef3d7ed389ffb9086143
Jeremiah Bautista <jeremiah.bautista@gmail.com>
2020-10-03 18:40:52 -0700

    fix(nav): adds basename prop to router; fixes prod routing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Navbar bg="light" expand="lg">
           <Navbar.Brand as={Link} to="/">
